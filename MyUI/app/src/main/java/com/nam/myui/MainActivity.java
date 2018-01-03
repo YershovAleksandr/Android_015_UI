71ef5243717c36e85f79cf13dd4dd541b9e1e999
hp <namstudionsk@gmail.com>
2018-01-03 22:48:28 +0700

    Add

--- MyUI/app/src/main/java/com/nam/myui/MainActivity.java
+++ MyUI/app/src/main/java/com/nam/myui/MainActivity.java
@@ -4,10 +4,15 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.nam.myui2.x42;
+
 public class MainActivity extends AppCompatActivity {
+
+    public String LOGTAG = "X42";
 
     private TextView mTextMessage;
 
@@ -39,6 +44,12 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+        x42 xx = new x42();
+
+        Log.i(LOGTAG, "Fuck year!!! " + xx.mX42 );
+
     }
 
 }
