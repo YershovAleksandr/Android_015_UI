e79ca22689ca7220ee34233204298925a67feaf8
hp <namstudionsk@gmail.com>
2018-01-06 23:17:35 +0700

    UI Table Layout fix

---
+++ MyUI/app/src/main/java/com/nam/myui/TableLayoutActivity.java
@@ -0,0 +1,12 @@
+package com.nam.myui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TableLayoutActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.uitablelayout);
+    }
+}
